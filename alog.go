0728c4b64e675756344add0402d6b275c0991664
kangkyu <kangkyu1111@gmail.com>
2022-01-09 03:57:11 -0800

    wg.Done() even when panic

--- alog.go
+++ alog.go
@@ -64,6 +64,7 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	al.m.Lock()
 	defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
@@ -72,7 +73,6 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 			al.errorCh <- err
 		}(err)
 	}
-	wg.Done()
 }
 
 func (al Alog) shutdown() {
