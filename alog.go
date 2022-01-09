17f0cbc95391fd204a44f0a3c546a376aa727e80
kangkyu <kangkyu1111@gmail.com>
2022-01-09 03:15:59 -0800

    Pass err as argument

--- alog.go
+++ alog.go
@@ -63,9 +63,9 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
   defer al.m.Unlock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		go func() {
+		go func(err error) {
 			al.errorCh <- err
-		}()
+		}(err)
 	}
 }
 
